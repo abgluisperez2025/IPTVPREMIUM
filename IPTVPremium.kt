1e166952ab3a2b08838be41be9742bea997cc46d
LUIS PEREZ <abgluisperez@gmail.com>
2026-05-24 17:51:05 -0500

    Delete IPTVPremium.kt

--- IPTVPremium.kt
+++
@@ -1,7 +0,0 @@
-class IPTVPremium : MainAPI() {
-
-override var mainUrl =
-    "http://kazan-tv.com:8091"
-
-private val playlistUrl =
-    "http://kazan-tv.com:8091/get.php?username=AlbertoMi&password=FTud8386d&type=m3u_plus"
