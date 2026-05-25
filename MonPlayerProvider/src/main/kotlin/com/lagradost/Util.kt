1f56ad4c57453dd9d4f521526c10637d016e9d26
LUIS PEREZ <abgluisperez@gmail.com>
2026-05-24 21:52:33 -0500

    Update Util.kt

--- MonPlayerProvider/src/main/kotlin/com/lagradost/Util.kt
+++ MonPlayerProvider/src/main/kotlin/com/lagradost/Util.kt
@@ -1,19 +1,19 @@
 package com.anhdaden
 
-fun String.removeVietnameseAccents(): String {
-    val vietnameseAccents = mapOf(
-        "àáạảãâầấậẩẫăằắặẳẵ" to "a",
-        "èéẹẻẽêềếệểễ" to "e",
-        "ìíịỉĩ" to "i",
-        "òóọỏõôồốộổỗơờớợởỡ" to "o",
-        "ùúụủũưừứựửữ" to "u",
-        "ỳýỵỷỹ" to "y",
-        "đ" to "d"
+fun String.removerAcentos(): String {
+    val mapaAcentos = mapOf(
+        "áàäâ" to "a",
+        "éèëê" to "e",
+        "íìïî" to "i",
+        "óòöô" to "o",
+        "úùüû" to "u",
+        "ñ" to "n"
     )
-    var result = this.lowercase()
-    for ((key, value) in vietnameseAccents) {
-        result = result.replace(Regex("[$key]"), value)
+    
+    var resultado = this.lowercase()
+    for ((key, value) in mapaAcentos) {
+        resultado = resultado.replace(Regex("[$key]"), value)
     }
  
-    return result
+    return resultado
 }
